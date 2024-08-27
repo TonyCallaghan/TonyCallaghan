b6c269371465297b9b020f5378da805d1efc4941
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2024-08-27 09:48:47 +0100

    updating readme.md

--- script.js
+++ script.js
@@ -22,14 +22,10 @@ const readme = `\
   <a href="mailto:a.tc.callaghan@gmail.com">
     <img src="https://img.shields.io/badge/-a.tc.callaghan@gmail.com-c14438?style=flat&logo=Gmail&logoColor=white&link=mailto:a.tc.callaghan@gmail.com" alt="Gmail Badge">
   </a>
-  <a href="https://hellotony.me">
-    <img src="https://img.shields.io/badge/-HelloTony.me-22333d?style=flat&logo=nodered&logoColor=43f0c7&link=https://hellotony.me" alt="Website Badge">
-  </a>
 </p>
 
-- I'm currently a 3<sup>rd</sup> year Computer Science & Software Engineering student in Maynooth University. 
-- I'm looking for internship opportunities in 2024 as part of my course. 
-- Please check out my [previous experience](https://hellotony.me/Docs/Tony_Callaghan_CV.pdf).
+- I'm currently a 4<sup>th</sup> year Computer Science & Software Engineering student in Maynooth University.
+- Highly interested in software engineering & cloud computing.
 
 ---
 
